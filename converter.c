1ea4181b520d6e08c370360a0c9ea14abf6bf67b
Ouail Derghal <ouailderghal1@gmail.com>
2023-02-08 20:49:53 +0100

    Parse file into StringView type

--- converter.c
+++ converter.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <assert.h>
 
+#define SV_IMPLEMENTATION
+#include "sv.h"
+
 char *read_file(const char *file_path, size_t *out_file_size) {
   FILE *file = fopen(file_path, "rb");
   char *file_buffer = NULL;
@@ -64,7 +67,12 @@ int main(int argc, char *argv[]) {
   size_t file_size = 0;
   char *file_content = read_file(file_path, &file_size);
 
-  fwrite(file_content, 1, file_size, stdout);
+  String_View content = {
+    .count = file_size,
+    .data = file_content,
+  };
+
+  printf(SV_Fmt "", SV_Arg(content));
 
   return EXIT_SUCCESS;
 }
